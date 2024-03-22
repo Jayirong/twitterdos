a473c85585c32f2086b4888fd990f2cc0fd4c79c
Jayirong <lemonsoft5555@gmail.com>
2024-03-21 22:57:56 -0300

    primer commit

---
+++ src/main/java/com/twitter/twitterdos/TwitterdosApplication.java
@@ -0,0 +1,13 @@
+package com.twitter.twitterdos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterdosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterdosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twitter/twitterdos/TwitterdosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.twitterdos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterdosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
