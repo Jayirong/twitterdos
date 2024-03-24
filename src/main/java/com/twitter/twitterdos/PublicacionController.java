ad5e19c7432dcd13fb06479741325b98eff44124
Jayirong <lemonsoft5555@gmail.com>
2024-03-24 18:49:29 -0300

    ajustes menores

--- src/main/java/com/twitter/twitterdos/PublicacionController.java
+++ src/main/java/com/twitter/twitterdos/PublicacionController.java
@@ -8,11 +8,9 @@ import java.util.LinkedHashMap;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
