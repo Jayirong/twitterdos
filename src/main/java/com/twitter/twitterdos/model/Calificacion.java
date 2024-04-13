7630ba512ea7b8654e108f0b4a6c5f1c818d9cc7
Jayirong <lemonsoft5555@gmail.com>
2024-04-13 18:21:18 -0400

    Creacion modelo de Calificacion y Comentario

--- src/main/java/com/twitter/twitterdos/model/Calificacion.java
+++ src/main/java/com/twitter/twitterdos/model/Calificacion.java
@@ -1,5 +1,55 @@
 package com.twitter.twitterdos.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "calificacion")
 public class Calificacion {
+    //se definen los atributos del objeto "comentario"
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
     
+    @Column(name = "nota")
+    private float nota;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pubicacion")
+    private Publicacion publicacion;
+
+    //Getters
+    public Long getId(){
+        return id;
+    }
+
+    public float getNota(){
+        return nota;
+    }
+
+    public Publicacion getPublicacion(){
+        return publicacion;
+    }
+
+    //Setters
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setNota(float nota){
+        this.nota = nota;
+    }
+
+    public void setPublicacion(Publicacion publicacion){
+        this.publicacion = publicacion;
+    }
+
 }
--- src/main/java/com/twitter/twitterdos/model/Comentario.java
+++ src/main/java/com/twitter/twitterdos/model/Comentario.java
@@ -1,31 +1,67 @@
 package com.twitter.twitterdos.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "comentario")
 public class Comentario {
-    //se definen los atributos del objeto "coemntario"
-    private int id;
+    //se definen los atributos del objeto "comentario"
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "texto")
     private String texto;
+    
+    @Column(name = "autor")
     private String autor;
 
-    //constructor
-    public Comentario(int id, String texto, String autor){
-        this.id = id;
-        this.texto = texto;
-        this.autor = autor;
-    }
+    @ManyToOne
+    @JoinColumn(name = "id_publicacion")
+    private Publicacion publicacion;
 
+    
     //getters
-    public int getIdComentario(){
+    public Long getId(){
         return id;
     }
 
-    public String getTextoComentario(){
+    public String getTexto(){
         return texto;
     }
 
-    public String getAutorComentario(){
+    public String getAutor(){
         return autor;
     }
 
+    public Publicacion getPublicacion(){
+        return publicacion;
+    }
 
+    //Setters
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setTexto(String texto){
+        this.texto = texto;
+    }
+
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
+
+    public void setPublicacion(Publicacion publicacion){
+        this.publicacion = publicacion;
+    }
 
 }
