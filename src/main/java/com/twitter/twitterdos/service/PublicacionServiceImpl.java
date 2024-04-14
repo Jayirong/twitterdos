7213a9a3698bb4c9f5fd3e2643510ffdf5cc249d
Jayirong <Lemonsoft5555@gmail.com>
2024-04-13 23:30:04 -0400

    Service Publicacion

--- src/main/java/com/twitter/twitterdos/service/PublicacionServiceImpl.java
+++ src/main/java/com/twitter/twitterdos/service/PublicacionServiceImpl.java
@@ -1,5 +1,52 @@
 package com.twitter.twitterdos.service;
 
-public class PublicacionServiceImpl {
+import com.twitter.twitterdos.model.Publicacion;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.twitter.twitterdos.repository.PublicacionRepository;
+
+@Service
+public class PublicacionServiceImpl implements PublicacionService{
+    @Autowired
+    private PublicacionRepository publicacionRepository;
+
+    @Override
+    public Publicacion createPublicacion(Publicacion publicacion){
+        return publicacionRepository.save(publicacion);
+    }
+
+    @Override
+    public List<Publicacion> getPublicaciones(){
+        return publicacionRepository.findAll();
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    public Optional<Publicacion> getPublicacionById(Long id){
+        return publicacionRepository.findById(id);
+    }
+
+    @Override
+    public Publicacion updatePublicacion(Long id, Publicacion publicacion){
+        if(publicacionRepository.existsById(id)){
+            publicacion.setId(id);
+            return publicacionRepository.save(publicacion);
+        }
+        else{
+            return null;
+        }
+    }
+
+    @Override
+    public void deletePublicacion(Long id){
+        publicacionRepository.deleteById(id);
+    }
     
+
+
 }
