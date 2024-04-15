06d36b01ea26fbda7778745978f3f09596d125d4
Jayirong <lemonsoft5555@gmail.com>
2024-04-15 00:36:42 -0400

    Adicion de controlador generico para labores varias

---
+++ src/main/java/com/twitter/twitterdos/controller/GenericController.java
@@ -0,0 +1,41 @@
+package com.twitter.twitterdos.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.twitter.twitterdos.model.Calificacion;
+import com.twitter.twitterdos.service.CalificacionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping("/api")
+public class GenericController {
+    @Autowired
+    private CalificacionService calificacionService;
+
+    private double calcularPromedio(List<Calificacion> calificaciones){
+        double sumatoria = calificaciones.stream()
+                            .mapToDouble(Calificacion::getNota)
+                            .sum();
+        return sumatoria / calificaciones.size();
+    }
+
+
+    @GetMapping("/publicaciones/{id}/promedio-calificaciones")
+    public ResponseEntity<Double> getPromedioCalificaciones(@PathVariable Long id) {
+        List<Calificacion> calificaciones = calificacionService.getCalificacionesByPublicacion(id);
+        
+        if (calificaciones.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        double promedio = calcularPromedio(calificaciones);
+        return ResponseEntity.ok(promedio);
+    }
+    
+    
+}
